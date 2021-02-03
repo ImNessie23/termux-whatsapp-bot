7430addb1f5bca5775e75547a554845f0f1ea62a
ナスワサルサ <75057738+NazwaS@users.noreply.github.com>
2021-02-03 22:18:48 +0700

    Update index.js

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ CFonts.say('\'termux-whatsapp-bot\' By @ImNessie23 & @NazwaS', {
 })
 
 function start() {
-  let args = [path.join(__dirname, 'nzwa.js'), ...process.argv.slice(2)]
+  let args = [path.join(__dirname, 'nessie.js'), ...process.argv.slice(2)]
   CFonts.say([process.argv[0], ...args].join(' '), {
     font: 'console',
     align: 'center',
