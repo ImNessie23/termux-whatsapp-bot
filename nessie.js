8333d2e113a77f5299625a237e02dd14cb02f748
ナスワサルサ <75057738+NazwaS@users.noreply.github.com>
2021-02-03 22:40:08 +0700

    Update nessie.js

--- nessie.js
+++ nessie.js
@@ -6,3 +6,11 @@ const {
     Mimetype,
     GroupSettingChange
 } = require('@adiwajshing/baileys')
+const { color, bgcolor } = require('./lib/color')
+const { wait, simih, getBuffer, h2k, generateMessageID, getGroupAdmins, getRandom, start, info, success, close } = require('./lib/functions')
+const { fetchJson } = require('./lib/fetcher')
+const { recognize } = require('./lib/ocr')
+const fs = require('fs')
+const moment = require('moment-timezone')
+const { exec } = require('child_process')
+const fetch = require('node-fetch')
