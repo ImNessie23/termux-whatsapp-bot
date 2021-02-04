51e8d017b3be0da96a884c82ddef9ac64a7f46b1
ナスワサルサ <75057738+NazwaS@users.noreply.github.com>
2021-02-04 16:38:50 +0700

    Update nessie.js

--- nessie.js
+++ nessie.js
@@ -155,6 +155,14 @@ async function starts() {
 					buffer = await getBuffer(me.imgUrl)
 					client.sendMessage(from, buffer, image, {caption: teks, contextInfo:{mentionedJid: [me.jid]}})
 					break
+				case 'blocklist':
+					teks = 'This is list of blocked number :\n'
+					for (let block of blocked) {
+						teks += `➽ @${block.split('@')[0]}\n`
+					}
+					teks += `➽ *Total* : ${blocked.length}`
+					client.sendMessage(from, teks.trim(), extendedText, {quoted: mek, contextInfo: {"mentionedJid": blocked}})
+					break
 				default:
 					if (isGroup && isSimi && budy != undefined) {
 						console.log(budy)
